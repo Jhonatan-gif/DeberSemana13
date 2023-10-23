9af4e4b3bd70c4b75db716b5c6807fe0c23955b7
Jhonatan Tipan <jhonatan.tipan@udla.edu.ec>
2023-10-22 23:01:09 -0500

    Impresion de los datos

--- Ejercicio43/Ejercicio43.c
+++ Ejercicio43/Ejercicio43.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int fib1 = 0, fib2 = 1, fib3 = 1;
     int count = 0;
 
-    while (fib3 <= 2000) {
-        if (fib3 >= 1000) {
+    while (fib3 <= 2000)
+    {
+        if (fib3 >= 1000)
+        {
             count++;
         }
-        
+
         fib1 = fib2;
         fib2 = fib3;
         fib3 = fib1 + fib2;
     }
-    
+
+    printf("La cantidad de elementos de la serie de Fibonacci entre 1000 y 2000 es: %d\n", count);
+
     return 0;
 }
