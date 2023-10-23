516bdbbdef85b8a404ac07ed309f56d99456f5c6
Jhonatan Tipan <jhonatan.tipan@udla.edu.ec>
2023-10-22 19:50:28 -0500

    Analisis y ejecucion del proceso del ejercicio

--- Ejercicio16/Ejercicio16.c
+++ Ejercicio16/Ejercicio16.c
@@ -3,10 +3,20 @@
 int main()
 {
     int n;
+    int i;
+    int suma = 0;
 
     printf ("Ingrese el valor de n : ");
     scanf("%d", &n);//leer numero
 
+    if (n <= 0) {
+        printf("El valor de n debe ser un número positivo.\n");
+    } else {
+        for (i = 1; i <= n; i++) {
+            suma += 3 * i;
+        }
+    }
+    int promedio = suma / n;
 
 
     return 0;
