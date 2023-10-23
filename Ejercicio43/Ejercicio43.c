b5c2e34b9d05b2c92508bc12923bf30b2b4513b5
Jhonatan Tipan <jhonatan.tipan@udla.edu.ec>
2023-10-22 22:59:44 -0500

    Realizacion del proceso y escojer el mejor ciclo

--- Ejercicio43/Ejercicio43.c
+++ Ejercicio43/Ejercicio43.c
@@ -4,5 +4,15 @@ int main() {
     int fib1 = 0, fib2 = 1, fib3 = 1;
     int count = 0;
 
+    while (fib3 <= 2000) {
+        if (fib3 >= 1000) {
+            count++;
+        }
+        
+        fib1 = fib2;
+        fib2 = fib3;
+        fib3 = fib1 + fib2;
+    }
+    
     return 0;
 }
